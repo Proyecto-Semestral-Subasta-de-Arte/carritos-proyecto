2040f4be9bddeb59b161a6b5d8683eb3d1f86a89
Usuario <geral.gonzalezs@duocuc.cl>
2026-05-06 21:47:53 -0400

    Desarrollo modelo Carrito

--- src/main/java/cl/sda1085/carritos/model/Carrito.java
+++ src/main/java/cl/sda1085/carritos/model/Carrito.java
@@ -1,4 +1,46 @@
 package cl.sda1085.carritos.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "carritos")
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Carrito {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long idUsuario;
+
+    @Column(nullable = false)
+    private Long idProducto;
+
+    @Column(nullable = false)
+    private Integer cantidad;
+
+    @Column(nullable = false, precision = 12, scale = 2)
+    private BigDecimal precioUnitario;
+
+    @Column(nullable = false)
+    private LocalDateTime fechaAgregado;
+
+    @Column(nullable = false)
+    private String estado; // activo, comprado y abandonado
+
+    @PrePersist
+    protected void onCreate() {
+        this.fechaAgregado = LocalDateTime.now();
+        if (this.estado == null) this.estado = "ACTIVO";
+    }
 }
